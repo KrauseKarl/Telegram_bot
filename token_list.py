507c756a39c17f1e7d9780494903121acd27d52f
KrauseKarl <102583708+KrauseKarl@users.noreply.github.com>
2022-03-30 01:18:04 +0500

    Delete token_list.py

--- token_list.py
+++
@@ -1,8 +0,0 @@
-token_bot = '5117488486:AAF0B4ugDyroUd5jaNXfWyhruHJAm8UNHc0'
-
-detected_headers = {
-    "content-type": "application/x-www-form-urlencoded",
-    "Accept-Encoding": "application/gzip",
-    "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-    "X-RapidAPI-Key": "daa3df1d6bmsh0c6eb076b968e76p13f645jsn726c14641f04"
-}
